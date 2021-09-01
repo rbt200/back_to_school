fa6084aaeafc642a7e42123d0e3db7380f33b496
rbt200 <myesoap@mail.ru>
2021-09-01 23:29:13 +0300

    Finish project Meal Finder

--- meal_finder/script.js
+++ meal_finder/script.js
@@ -59,6 +59,21 @@ function getMealById(mealID) {
     });
 }
 
+// Fetch random meal from API
+function getRandomMeal() {
+    // Clear meals and headings
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+
+            addMealToDOM(meal);
+        });
+}
+
 // Add meal to DOM
 function addMealToDOM(meal) {
   const ingredients = [];
@@ -93,6 +108,7 @@ function addMealToDOM(meal) {
 }
 
 submit.addEventListener("submit", searchMeal);
+random.addEventListener("click", getRandomMeal);
 
 mealsEl.addEventListener("click", (e) => {
   const mealInfo = e.path.find((item) => {
