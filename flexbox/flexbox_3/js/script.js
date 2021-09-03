02cc33210e95ec02eda61e7e6810222f10653b00
rbt200 <myesoap@mail.ru>
2021-09-03 20:44:06 +0300

    Finish project Flexbox_3

--- flexbox/flexbox_3/js/script.js
+++ flexbox/flexbox_3/js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+    $("#magnify").click(function () {
+      $(this).hide();
+      $("#search").toggle("scale");
+      $("search").css("dispalay", "block");
+    });
 
 });
 
