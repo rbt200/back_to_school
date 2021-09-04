c32a27d12af828d4def0b9c1434708d2d87ab8db
rbt200 <myesoap@mail.ru>
2021-09-04 22:50:13 +0300

    Create HTML for Flexbox 4

--- flexbox/flexbox_4/script.js
+++ flexbox/flexbox_4/script.js
@@ -1,10 +1,2 @@
-$(document).ready(function() {
 
-    $("#magnify").click(function () {
-      $(this).hide();
-      $("#search").toggle("scale");
-      $("search").css("dispalay", "block");
-    });
-
-});
 
