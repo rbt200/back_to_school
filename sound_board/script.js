2f80318b2cd00ca6daa675be58fe3d1d25433133
rbt200 <myesoap@mail.ru>
2021-09-25 19:34:24 +0300

    Finish project Sound Board

---
+++ sound_board/script.js
@@ -0,0 +1,29 @@
+const sounds = ["melody_1", "melody_2", "melody_6", "melody_7", "melody_8"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  const stopBtn = document.getElementById("stop");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  document.getElementById("buttons").appendChild(btn);
+
+  btn.addEventListener("click", () => {
+    stopSounds();
+    document.getElementById(sound).play();
+  });
+
+  stopBtn.addEventListener("click", () => {
+    stopSounds();
+  });
+});
+
+function stopSounds() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+}
